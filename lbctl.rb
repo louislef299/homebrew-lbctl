133f542bef56c46bb8f6754b19134b0c5f5c573d
Louis Lefebvre <lefebl4@medtronic.com>
2022-06-23 13:59:27 -0500

    update sha256

--- lbctl.rb
+++ lbctl.rb
@@ -2,7 +2,7 @@ class Lbctl < Formula
     desc "Lbctl is a cli tool for little bitta"
     homepage "https://github.com/louislef299/lbctl"
     url "https://github.com/louislef299/lbctl/archive/refs/tags/v0.1.tar.gz"
-    sha256 "/acZwrzdPU9wkXQQFjsTQX6R3mpVDaKKoKFpzn71i+Q lefebl4@LEFEBL4-Y4CD47VXQ5"
+    sha256 "623bbea7fd483281edd01d54455b342a41b94b8513b57ac113a0f46007d39197"
     license "MIT"
   
     depends_on "go"
